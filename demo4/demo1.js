8a13e9d34ce00750b931285964e0cb55ccc2d7c6
yjc <756319278@qq.com>
2018-04-04 18:50:19 +0800

    update

--- demo4/demo1.js
+++ demo4/demo1.js
@@ -2,22 +2,45 @@ const express = require('express')
 const cheerio = require('cheerio')
 const superagent = require('superagent')
 const url = require('url')
+const eventproxy = require('eventproxy')
 
 const app = express()
 const cnodeUrl = 'https://cnodejs.org/'
 
-superagent.get(cnodeUrl).end((err, sres) => {
-  if(err) return console.error(err)
-  let topicUrls = []
-  let $ = cheerio.load(sres.text)
-  $('#topic_list .topic_title').each((idx, element) => {
-    let $element = $(element)
-    let href = url.resolve(cnodeUrl, $element.attr('href'))
-    topicUrls.push(href)
+app.get('/', (req, res, next) => {
+  superagent.get(cnodeUrl).end((err, sres) => {
+    if(err) return next(err)
+    let topicUrls = []
+    let $ = cheerio.load(sres.text)
+    $('#topic_list .topic_title').each((idx, element) => {
+      let $element = $(element)
+      let href = url.resolve(cnodeUrl, $element.attr('href'))
+      topicUrls.push(href)
+    })
+    let ep = new eventproxy()
+    // ep.after https://github.com/JacksonTian/eventproxy#%E9%87%8D%E5%A4%8D%E5%BC%82%E6%AD%A5%E5%8D%8F%E4%BD%9C
+    ep.after('topic_html', topicUrls.length, (topics) => {
+      topics = topics.map((topicPair) => {
+        let topicUrl = topicPair[0]
+        let topicHtml = topicPair[1]
+        let $ = cheerio.load(topicHtml)
+        return ({
+          title: $('.topic_full_title').text().trim(),
+          href: topicUrl,
+          comment1: $('.reply_content').eq(0).text().trim()
+        })
+      })
+      res.send(topics)
+    })
+    topicUrls.forEach((topicUrl) => {
+      superagent.get(topicUrl).end((err, sres) => {
+        console.log('fetch ' + topicUrl + ' successful');
+        ep.emit('topic_html', [topicUrl, sres.text]);
+      })
+    })
   })
-  console.log(topicUrls)
 })
 
 app.listen(3000, () => {
-  'app is listening at port 3000'
+  console.log('app is listening at port 3000')
 })
